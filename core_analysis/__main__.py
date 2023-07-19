8386a7737746be57d05cc40236ebb1ed96c5e247
Jérome Simon <jerome.simon@inrs.ca>
2023-07-19 15:30:55 -0400

    Update main script

--- core_analysis/__main__.py
+++ core_analysis/__main__.py
@@ -5,13 +5,12 @@ from argparse import ArgumentParser
 
 os.environ["SM_FRAMEWORK"] = "tf.keras"
 
-import numpy as np
 import tensorflow as tf
 
+from core_analysis.dataset import Dataset
 from core_analysis.architecture import Model
-from core_analysis.dataset import prepare_inputs, prepare_test_inputs
 from core_analysis.utils.visualize import plot_loss, plot_predictions, plot_test_results
-from core_analysis.utils.constants import LOAD_FILENAME, LABELS_PATH
+from core_analysis.utils.constants import MODEL_FILENAME, LABELS_PATH
 
 
 # Check the number of available GPUs.
@@ -26,27 +25,26 @@ parser = ArgumentParser()
 parser.add_argument("--train", action="store_true")
 parser.add_argument("--test", action="store_true")
 parser.add_argument("-p", "--plot", action="store_true")
-parser.add_argument("-w", "--weights-filename", default=LOAD_FILENAME)
+parser.add_argument("-w", "--weights-filename", default=MODEL_FILENAME)
 parser.add_argument("-a", "--do_augment", action="store_true")
 
 
 def main(args):
     model = Model()
+    dataset = Dataset(LABELS_PATH)
 
     if args.train:
-        dataset = Dataset(LABELS_PATH)
-        X_train, Y_train, X_test, Y_test = prepare_inputs(args.do_augment, args.do_plot)
-        history = model.train(X_train, Y_train, X_test, Y_test)
+        history = model.train(
+            dataset.subset("train"), dataset.subset("val"), args.weights_filename
+        )
         if args.plot:
             plot_loss(history)
-            plot_predictions(model, X_test, Y_test, begin=600, end=610)
+            plot_predictions(model, dataset.subset("val"), begin=600, end=610)
 
     if args.test:
-        images, mask = prepare_test_inputs()
-        results = model.test(images)
-        results[mask] = 0.0
+        results = model.test(dataset.subset("test"))
         if args.plot:
-            plot_test_results(images, results)
+            plot_test_results(results)
 
 
 if __name__ == "__main__":
