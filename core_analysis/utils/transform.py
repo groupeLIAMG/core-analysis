ffb86e42302c81933866c4d72a757051ca4def13
Jérome Simon <jerome.simon@inrs.ca>
2023-07-18 22:55:01 -0400

    Optimize transforms

--- core_analysis/utils/transform.py
+++ core_analysis/utils/transform.py
@@ -11,10 +11,6 @@ from imgaug.augmenters import (
 )
 
 
-def normalize(x, lim=255.0):
-    return (x - np.nanmin(x)) / (np.nanmax(x) - np.nanmin(x)) * lim
-
-
 def adjust_rgb(img, perc_init=5, perc_final=95, nchannels=3):
     dim = img.shape
     adjusted_img = np.zeros((dim))
@@ -22,8 +18,8 @@ def adjust_rgb(img, perc_init=5, perc_final=95, nchannels=3):
     if dim[-1] == nchannels:
         for n in range(nchannels):
             channel = img[:, :, n]
-            perc_i = np.nanpercentile(channel, perc_init)
-            perc_f = np.nanpercentile(channel, perc_final)
+            perc_i = np.percentile(channel, perc_init)
+            perc_f = np.percentile(channel, perc_final)
             channel = np.clip(channel, perc_i, perc_f)
             channel = normalize(channel, 1.0)
             adjusted_img[:, :, n] = channel
@@ -34,99 +30,43 @@ def adjust_rgb(img, perc_init=5, perc_final=95, nchannels=3):
     return adjusted_img
 
 
+def normalize(x, lim=255.0):
+    return (x - np.min(x)) / (np.max(x) - np.min(x)) * lim
+
+
 def min_dist(x_n, y_n, pairs):
-    distances = [999999.0]
-    if len(pairs) > 0:
-        distances = []
-        for pair in pairs:
-            x_f, y_f = pair
-            distances.append(np.sqrt(np.power(x_n - x_f, 2) + np.power(y_n - y_f, 2)))
-
-    return np.min(distances)
-
-
-def hillshade(array, azimuth, angle_altitude):
-    x, y = np.gradient(array)
-    slope = np.pi / 2.0 - np.arctan(np.sqrt(x * x + y * y))
-    aspect = np.arctan2(-x, y)
-    azimuthrad = azimuth * np.pi / 180.0
-    altituderad = angle_altitude * np.pi / 180.0
-
-    shaded = np.sin(altituderad) * np.sin(slope) + np.cos(altituderad) * np.cos(
-        slope
-    ) * np.cos(azimuthrad - aspect)
-    hillshade_array = 255 * (shaded + 1) / 2
-
-    return hillshade_array
-
-
-def standardize_data(grid, data_format="channels_last"):
-    dim = grid.shape
-    std_grid = np.zeros(dim)
-
-    for n in range(np.min(dim)):
-        if data_format == "channels_last":
-            data = (grid[:, :, n] - np.nanmean(grid[:, :, n])) / np.nanstd(
-                grid[:, :, n]
-            )
-            std_grid[:, :, n] = (data - np.nanmin(data)) / (
-                np.nanmax(data) - np.nanmin(data)
-            )
-
-        if data_format == "channels_first":
-            data = (grid[n] - np.nanmean(grid[n])) / np.nanstd(grid[n])
-            std_grid[n] = (data - np.nanmin(data)) / (np.nanmax(data) - np.nanmin(data))
-
-    return std_grid
+    min_distance = np.inf
+    for x_f, y_f in pairs:
+        distance = np.sqrt((x_n - x_f) ** 2 + (y_n - y_f) ** 2)
+        if distance < min_distance:
+            min_distance = distance
+    return min_distance
 
 
 def undersample(image, mask=None, undersample_by=2):
-    yy = np.arange(0, image.shape[0], undersample_by)
-    xx = np.arange(0, image.shape[1], undersample_by)
-
-    idx, idy = np.meshgrid(xx, yy)
-
-    ny = idy.shape[0]
-    nx = idy.shape[1]
-
-    image = image[idy.ravel(), idx.ravel(), :]
-    image = image.reshape((ny, nx, 3))
-
+    image = image[::undersample_by, ::undersample_by]
     if mask is not None:
-        mask = mask[idy.ravel(), idx.ravel(), :]
-        mask = mask.reshape((ny, nx, mask.shape[-1]))
-
+        mask = mask[::undersample_by, ::undersample_by]
     return image, mask
 
 
-def upsample(image, original_image, result):
-    y0 = np.arange(image.shape[0])
-    x0 = np.arange(image.shape[1])
-
+def upsample(arr, dest_shape):
+    y0 = np.arange(arr.shape[0])
+    x0 = np.arange(arr.shape[1])
     x0, y0 = np.meshgrid(x0, y0)
 
-    y = np.linspace(y0.min(), y0.max(), original_image.shape[0])
-    x = np.linspace(x0.min(), x0.max(), original_image.shape[1])
-
+    y = np.linspace(y0.min(), y0.max(), dest_shape[0])
+    x = np.linspace(x0.min(), x0.max(), dest_shape[1])
     x, y = np.meshgrid(x, y)
 
-    interp_result = griddata(
+    arr = griddata(
         (x0.ravel(), y0.ravel()),
-        result[:, :, 0].ravel(),
+        arr[:, :, 0].ravel(),
         (x, y),
         method="nearest",
     )
 
-    return interp_result
-
-
-def return_zeroed(mask, crf_mask):
-    _, _, nz = mask.shape
-    for z in range(nz):
-        summ = np.sum(mask[:, :, z])
-        if summ == 0:
-            crf_mask[:, :, z] = 0.0
-    return crf_mask
+    return arr
 
 
 augment = Sequential(
