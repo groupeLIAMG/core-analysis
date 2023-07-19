a33a71a5d893b04df5fae759ac66504007174b3b
Jérome Simon <jerome.simon@inrs.ca>
2023-07-19 13:03:46 -0400

    Fix stored properties

--- core_analysis/utils/processing.py
+++ core_analysis/utils/processing.py
@@ -2,8 +2,8 @@
 
 
 class stored_property(property):
-    def __get__(self, obj):
-        name = "_" + self._name
+    def __get__(self, obj, objtype=None):
+        name = "_" + self.fget.__name__
         if not hasattr(obj, name):
-            setattr(obj, name, self.fget())
+            setattr(obj, name, self.fget(obj))
         return getattr(obj, name)
