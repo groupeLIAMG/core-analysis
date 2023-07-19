7f4019f608d56f72153af8db0c74ba6b03132d56
Jérome Simon <jerome.simon@inrs.ca>
2023-07-19 14:57:00 -0400

    Fix constants

--- core_analysis/utils/constants.py
+++ core_analysis/utils/constants.py
@@ -5,13 +5,15 @@ from datetime import date
 
 TODAY = str(date.today()).replace("-", "_")
 BATCH_SIZE = 500
-IMAGE_DIR = join("data", "images")
+IMAGE_DIR = join("images")
 PLOT_DIR = join("data", "plots")
 CHECKPOINT_DIR = join("data", "models", "save_models")
 MODEL_FILENAME = "linknet_efficientnetb7_weights_2023_07_05.h5"
 LABELS_FILENAME = "labels_20230714.json"
 LABELS_DIR = join("data", "json_files")
 LABELS_PATH = join(LABELS_DIR, LABELS_FILENAME)
+BACKGROUND_DIR = join("data", "backgrounds")
+MASK_DIR = join("data", "masks")
 LR = 1.5e-5
 DIM = (128, 128, 3)
 N_CLASSES = 3
